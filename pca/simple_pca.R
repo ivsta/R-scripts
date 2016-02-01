a5c5ca9e2beda9091c6cd58e37e945dc171114b9
wilsonpok <wilson.pok@gmail.com>
2016-02-01 11:40:34 +1100

    Add binning lines to plot

--- pca/simple_pca.R
+++ pca/simple_pca.R
@@ -34,3 +34,31 @@ ggplot(data = iris_res, aes(x = x, y = y, colour = Species)) +
 ggplot(data = iris_res, aes(x = x, y = y, colour = Sepal.Length)) + 
   geom_jitter(width = 0.3, height = 0.3) + theme_bw() +
   scale_colour_continuous(low = 'orange', high = 'blue')
+
+
+# Quantile binning
+quantile_x <- quantile(iris_res[, x])
+quantile_y <- quantile(iris_res[, y])
+
+ggplot(data = iris_res, aes(x = x, y = y, colour = Species)) + 
+  geom_jitter(width = 0.3, height = 0.3) + 
+  geom_vline(xintercept = quantile_x[1]) +
+  geom_vline(xintercept = quantile_x[2]) +
+  geom_vline(xintercept = quantile_x[3]) +
+  geom_vline(xintercept = quantile_x[4]) +
+  geom_hline(yintercept = quantile_y[1]) +
+  geom_hline(yintercept = quantile_y[2]) +
+  geom_hline(yintercept = quantile_y[3]) +
+  geom_hline(yintercept = quantile_y[4]) +
+  theme_bw()
+
+# More sensible binning
+ggplot(data = iris_res, aes(x = x, y = y, colour = Species)) + 
+  geom_jitter(width = 0.3, height = 0.3) + 
+  geom_vline(xintercept = 0.4) +
+  geom_vline(xintercept = 1.5) +
+  geom_vline(xintercept = 3) +
+  geom_hline(yintercept = 0.6) +
+  geom_hline(yintercept = 1.5) +
+  geom_hline(yintercept = 3.5) +
+  theme_bw()
