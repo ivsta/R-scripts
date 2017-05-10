4138d5de233a0fe84be0558c3d75d01ea65d7751
Wilson Pok <wilson.pok@ambiata.com>
2017-05-10 10:55:34 +1000

    Update

---
+++ svd/svd-iris.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(magrittr)
+
+set.seed(666)
+
+iris_train <- iris %>%
+  as_tibble() %>% 
+  sample_n(100)
+
+iris_test <- iris %>%
+  setdiff(iris_train)
+
+iris %>% nrow
+iris_train %>% nrow
+iris_test %>% nrow
+
+x.train <- iris_train %>% 
+  select(-Species)
+
+x.test <- iris_test %>% 
+  select(-Species)
+
+s <- svd( x.train, nu = 2, nv = 2 )
+
+x.test.embeddings  <- t(s$v) %*% as.matrix(t(x.test))
+
+t(x.test.embeddings) %*% t(s$v) %>% head
+
+x.test %>% head
